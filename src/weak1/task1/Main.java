c0fa6c6bbad963fb5d9c2f510b8ccb29035721e8
Garais Eduard <garais.eduard@gmail.com>
2022-09-18 16:19:58 +0300

    first commit

---
+++ src/weak1/task1/Main.java
@@ -0,0 +1,15 @@
+package weak1.task1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("What's your name?");
+
+        // Here we create obj. type Scanner and give him Standard input stream
+        Scanner scanner = new Scanner(System.in);
+        String UserName = scanner.nextLine();
+
+        System.out.printf("Hello, %s!", UserName);
+    }
+}
