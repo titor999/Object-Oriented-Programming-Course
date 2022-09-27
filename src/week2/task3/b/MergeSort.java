d6c3ecf4a92f17fb4ef539efe7b8d3d073aee4b0
Garais Eduard <garais.eduard@gmail.com>
2022-09-27 13:08:22 +0300

    Added week2/task3/b/MergeSort.java; week2/task3/c/Median.java

---
+++ src/week2/task3/b/MergeSort.java
@@ -0,0 +1,53 @@
+package week2.task3.b;
+
+public class MergeSort {
+    public static void mergeSort(int[] a, int n) {
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+
+        System.arraycopy(a, 0, l, 0, mid);
+        if (n - mid >= 0) System.arraycopy(a, mid, r, 0, n - mid);
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+
+        merge(a, l, r, mid, n - mid);
+    }
+
+    public static void merge(
+            int[] a, int[] l, int[] r, int left, int right) {
+
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (l[i] <= r[j]) {
+                a[k++] = l[i++];
+            }
+            else {
+                a[k++] = r[j++];
+            }
+        }
+        while (i < left) {
+            a[k++] = l[i++];
+        }
+        while (j < right) {
+            a[k++] = r[j++];
+        }
+    }
+
+    /* A utility function to print array of size n */
+    static void printArray(int[] arr)
+    {
+        for (int j : arr) System.out.print(j + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] actual = { 5, 1, 6, 2, 3, 4 };
+
+        MergeSort.mergeSort(actual, actual.length);
+        printArray(actual);
+    }
+}
