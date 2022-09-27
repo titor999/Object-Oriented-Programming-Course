eb7d8dc18cf4aa9530d07d752ccd9920cc9df3c4
Garais Eduard <garais.eduard@gmail.com>
2022-09-27 21:48:37 +0300

    fix changes

--- src/week2/task3/b/MergeSort.java
+++ src/week2/task3/b/MergeSort.java
@@ -19,8 +19,7 @@ public class MergeSort {
         merge(a, l, r, mid, n - mid);
     }
 
-    public static void merge(
-            int[] a, int[] l, int[] r, int left, int right) {
+    public static void merge(int[] a, int[] l, int[] r, int left, int right) {
 
         int i = 0, j = 0, k = 0;
         while (i < left && j < right) {
