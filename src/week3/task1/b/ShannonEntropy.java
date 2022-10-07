b588c7f95dc5da0bcc9d381b0d860c9ceffac7b7
Garais Eduard <garais.eduard@gmail.com>
2022-10-07 19:41:39 +0300

    added week3/task1/a/Main.java; week3/task1/b/ShannonEntropy.java; week3/task1/c/Main.java

---
+++ src/week3/task1/b/ShannonEntropy.java
@@ -0,0 +1,33 @@
+package week3.task1.b;
+import java.lang.Math;
+import java.util.Scanner;
+
+public class ShannonEntropy {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a string >>>");
+        String text =  scanner.nextLine();
+
+        System.out.printf("%.2f",shannonEntropy(text));
+
+    }
+
+    public static double shannonEntropy(String str) {
+        int len = str.length();
+        char[] frequency = new char[256];
+        for (char c : str.toCharArray()) {
+            frequency[c]++;
+        }
+        double entropy = 0;
+        for (int i = 0; i < 256; i++) {
+            int f = frequency[i];
+            if (f == 0) {
+                continue;
+            }
+            double k = (double)f / len;
+            entropy += k * Math.log(k) / Math.log(2);
+        }
+        return entropy * (-1);
+    }
+}
---
+++ src/week3/task1/c/Main.java
@@ -0,0 +1,43 @@
+package week3.task1.c;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the sensor reading >>>");
+        String sensorPackage = scanner.nextLine();
+
+        System.out.println("How to sort? (by height id / by average temperature rise) (1/2) >>>");
+        int sortingOption = scanner.nextInt();
+
+        String[] names = GetSensorPackage(sensorPackage, sortingOption);
+        for (String s: names) {
+            System.out.println(s);
+        }
+    }
+
+    public static String[] GetSensorPackage(String sensorPackage, int sortingOption) {
+        String[] arr = sensorPackage.split("@");
+
+        switch (sortingOption) {
+            case 1 -> {
+                Arrays.sort(arr); // ok
+                for (int i = 0; i < arr.length; i++) {
+                    arr[i] = arr[i].substring(0, 2) + " " + Double.parseDouble(arr[i].substring(2));
+                }
+            }
+            case 2 -> {
+                for (int i = 0; i < arr.length; i++) {
+                    arr[i] = arr[i].substring(2) + arr[i].substring(0, 2);
+                }
+                Arrays.sort(arr);
+                for (int i = 0; i < arr.length; i++) {
+                    arr[i] = arr[i].substring(2) + " " + Double.parseDouble(arr[i].substring(0, 2));
+                }
+            }
+        }
+        return arr;
+    }
+}
