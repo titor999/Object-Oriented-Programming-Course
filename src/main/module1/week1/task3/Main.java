d02490c4f07096c3e69f199699bfa18f1594774a
Garais Eduard <garais.eduard@gmail.com>
2022-10-11 16:38:35 +0300

    change directory project;

---
+++ src/main/module1/week1/task3/Main.java
@@ -0,0 +1,17 @@
+package main.module1.week1.task3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Input a length an edge of icosahedron:");
+        Scanner scanner = new Scanner(System.in);
+
+        double a = scanner.nextDouble();
+
+        // This is a search formula for an icosahedron volume
+        double V = 5 * Math.pow(a, 3) / 12 * (3 + Math.sqrt(5));
+
+        System.out.printf("V = %s cm3", V);
+    }
+}
--- src/week4/task3/WrongSensorPackageException.java
+++
@@ -1,7 +0,0 @@
-package week4.task3;
-
-public class WrongSensorPackageException extends RuntimeException {
-    public WrongSensorPackageException(String message) {
-        super(message);
-    }
-}
