07ed58af230e165bd0f9544f20ca48351f54c7d9
Garais Eduard <garais.eduard@gmail.com>
2022-10-12 16:12:03 +0300

    fix changes

--- src/main/module1/week4/task3/WrongSensorPackageException.java
+++ src/main/module1/week4/task3/WrongSensorPackageException.java
@@ -1,6 +1,7 @@
 package main.module1.week4.task3;
 
 public class WrongSensorPackageException extends RuntimeException {
+    // using in module1.week3.task1c
     public WrongSensorPackageException(String message) {
         super(message);
     }
